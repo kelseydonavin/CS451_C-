6b818c7cb787bcaac5086516450b7347e13cd474
Trenton Fales <trentafales@yahoo.com>
2020-04-25 18:20:12 -0700

    Working Set Current User and User Information Groups

--- CS451_Milestone1/Test.xaml.cs
+++ CS451_Milestone1/Test.xaml.cs
@@ -22,20 +22,138 @@ namespace CS451_Milestone1
     /// </summary>
     public partial class Test : Window
     {
-        private string username;
+        private string username = "";
+        private string userID;
 
         public Test()
         {
             InitializeComponent();
-            addUserID();
-            addColumnsToFriendsGrid();
+            //addUserID();
+            //addColumnsToFriendsGrid();
         }
 
-        private void Username_SelectionChanged(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            TextBox textBox = sender as TextBox;
-            username = textBox.Text;
+            this.username = Username.Text;
+            string sqlStr = "SELECT user_id FROM Users WHERE name = '" + username + "'";
+            executeQuery(sqlStr, addUserIDRow);
         }
+
+        private void userID_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (UserID.SelectedIndex > -1)
+            {
+                this.userID = UserID.SelectedItem.ToString();
+            }
+            string sqlStr = "SELECT name, average_stars, fans, yelping_since_time, funny, useful, cool," +
+                "tip_count, total_likes, latitude, longitude FROM Users WHERE user_id = '" + userID + "'";
+            executeQuery(sqlStr, addUserInformation);
+        }
+
+        private void addUserIDRow(NpgsqlDataReader R)
+        {
+            UserID.Items.Add(R.GetString(0));
+        }
+
+        private void addUserInformation(NpgsqlDataReader R)
+        {
+            Name.Text = R.GetString(0);
+            Stars.Text = R.GetDouble(1).ToString();
+            Fans.Text = R.GetInt16(2).ToString();
+            YelpingSince.Text = R.GetString(3);
+            Funny.Text = R.GetInt16(4).ToString();
+            Useful.Text = R.GetInt16(5).ToString();
+            Cool.Text = R.GetInt16(6).ToString();
+            TipCount.Text = R.GetInt16(7).ToString();
+            TotalTipLikes.Text = R.GetInt16(8).ToString();
+            Latitude.Text = R.GetDouble(9).ToString();
+            Longitude.Text = R.GetDouble(10).ToString();
+        }
+
+        //private void addColumnsToFriendsGrid()
+        //{
+        //    DataGridTextColumn col1 = new DataGridTextColumn();
+        //    col1.Binding = new Binding("Name");
+        //    col1.Header = "Name";
+        //    col1.Width = 100;
+        //    friendsGrid.Columns.Add(col1);
+
+        //    DataGridTextColumn col2 = new DataGridTextColumn();
+        //    col2.Binding = new Binding("TotalLikes");
+        //    col2.Header = "Total Likes";
+        //    col2.Width = 80;
+        //    friendsGrid.Columns.Add(col2);
+
+        //    DataGridTextColumn col3 = new DataGridTextColumn();
+        //    col3.Binding = new Binding("AvgStars");
+        //    col3.Header = "Avg Stars";
+        //    col3.Width = 80;
+        //    friendsGrid.Columns.Add(col3);
+
+        //    DataGridTextColumn col4 = new DataGridTextColumn();
+        //    col4.Binding = new Binding("YelpingSince");
+        //    col4.Header = "Yelping Since";
+        //    col4.Width = 232;
+        //    friendsGrid.Columns.Add(col4);
+        //}
+
+        //private void addUserID()
+        //{
+        //    using (var connection = new NpgsqlConnection(buildConnectionString()))
+        //    {
+        //        connection.Open();
+        //        using (var cmd = new NpgsqlCommand())
+        //        {
+        //            cmd.Connection = connection;
+        //            cmd.CommandText = "SELECT user_id FROM Users WHERE name = '" + username + "'";
+        //            try
+        //            {
+        //                var reader = cmd.ExecuteReader();
+        //                while (reader.Read())
+        //                    UserID.Text = reader.GetString(0);
+        //                    UserID.AppendText(Environment.NewLine);
+        //            }
+        //            catch (NpgsqlException ex)
+        //            {
+        //                Console.WriteLine(ex.Message.ToString());
+        //                System.Windows.MessageBox.Show("SQL Error - " + ex.Message.ToString());
+        //            }
+        //            finally
+        //            {
+        //                connection.Close();
+        //            }
+        //        }
+        //    }
+        //}
+
+        //private void addName()
+        //{
+        //    using (var connection = new NpgsqlConnection(buildConnectionString()))
+        //    {
+        //        connection.Open();
+        //        using (var cmd = new NpgsqlCommand())
+        //        {
+        //            cmd.Connection = connection;
+        //            cmd.CommandText = "SELECT Users.name FROM Friend INNER JOIN Users ON Friend.friend_id = Users.user_id " +
+        //                "WHERE Friend.user_id = '" + username + "'";
+        //            try
+        //            {
+        //                var reader = cmd.ExecuteReader();
+        //                while (reader.Read())
+        //                    friendsGrid.Items.Add(reader.GetString(0));
+        //            }
+        //            catch (NpgsqlException ex)
+        //            {
+        //                Console.WriteLine(ex.Message.ToString());
+        //                System.Windows.MessageBox.Show("SQL Error - " + ex.Message.ToString());
+        //            }
+        //            finally
+        //            {
+        //                connection.Close();
+        //            }
+        //        }
+        //    }
+        //}
 
         private string buildConnectionString()
         {
@@ -63,91 +181,6 @@ namespace CS451_Milestone1
                         Console.WriteLine("Could not connect to postgreSQL!");
                         Console.WriteLine(ex.Message.ToString());
                         System.Windows.MessageBox.Show("SQL Error -" + ex.Message.ToString());
-                    }
-                    finally
-                    {
-                        connection.Close();
-                    }
-                }
-            }
-        }
-
-        private void addColumnsToFriendsGrid()
-        {
-            DataGridTextColumn col1 = new DataGridTextColumn();
-            col1.Binding = new Binding("Name");
-            col1.Header = "Name";
-            col1.Width = 100;
-            friendsGrid.Columns.Add(col1);
-
-            DataGridTextColumn col2 = new DataGridTextColumn();
-            col2.Binding = new Binding("TotalLikes");
-            col2.Header = "Total Likes";
-            col2.Width = 80;
-            friendsGrid.Columns.Add(col2);
-
-            DataGridTextColumn col3 = new DataGridTextColumn();
-            col3.Binding = new Binding("AvgStars");
-            col3.Header = "Avg Stars";
-            col3.Width = 80;
-            friendsGrid.Columns.Add(col3);
-
-            DataGridTextColumn col4 = new DataGridTextColumn();
-            col4.Binding = new Binding("YelpingSince");
-            col4.Header = "Yelping Since";
-            col4.Width = 232;
-            friendsGrid.Columns.Add(col4);
-        }
-
-        private void addUserID()
-        {
-            using (var connection = new NpgsqlConnection(buildConnectionString()))
-            {
-                connection.Open();
-                using (var cmd = new NpgsqlCommand())
-                {
-                    cmd.Connection = connection;
-                    cmd.CommandText = "SELECT user_id FROM Users WHERE name = '" + username + "'";
-                    try
-                    {
-                        var reader = cmd.ExecuteReader();
-                        while (reader.Read())
-                            UserID.Text = reader.GetString(0);
-                            UserID.AppendText(Environment.NewLine);
-                    }
-                    catch (NpgsqlException ex)
-                    {
-                        Console.WriteLine(ex.Message.ToString());
-                        System.Windows.MessageBox.Show("SQL Error - " + ex.Message.ToString());
-                    }
-                    finally
-                    {
-                        connection.Close();
-                    }
-                }
-            }
-        }
-
-        private void addName()
-        {
-            using (var connection = new NpgsqlConnection(buildConnectionString()))
-            {
-                connection.Open();
-                using (var cmd = new NpgsqlCommand())
-                {
-                    cmd.Connection = connection;
-                    cmd.CommandText = "SELECT Users.name FROM Friend INNER JOIN Users ON Friend.friend_id = Users.user_id " +
-                        "WHERE Friend.user_id = '" + username + "'";
-                    try
-                    {
-                        var reader = cmd.ExecuteReader();
-                        while (reader.Read())
-                            friendsGrid.Items.Add(reader.GetString(0));
-                    }
-                    catch (NpgsqlException ex)
-                    {
-                        Console.WriteLine(ex.Message.ToString());
-                        System.Windows.MessageBox.Show("SQL Error - " + ex.Message.ToString());
                     }
                     finally
                     {
